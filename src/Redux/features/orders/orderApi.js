1cf7335f5d1abff7d3f13bd6adb67b29f8deacea
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-10-28 20:04:24 +0600

    order api

---
+++ src/Redux/features/orders/orderApi.js
@@ -0,0 +1,56 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getBaseUrl } from "../../../Utils/getBaseUrl";
+
+export const orderApi = createApi({
+  reducerPath: "orderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${getBaseUrl()}api/orders`,
+    credentials: "include",
+  }),
+  tagTypes: ["Orders"],
+  endpoints: (builder) => ({
+    getOrderByEmail: builder.query({
+      query: (email) => ({
+        url: `/${email}`,
+      }),
+      providesTags: ["Orders"],
+    }),
+
+    getOrderById: builder.query({
+      query: (id) => ({
+        url: `/order/${id}`,
+      }),
+      providesTags: (result, error, id) => [{ type: "Orders", id }],
+    }),
+
+    getAllOrders: builder.query({
+      query: () => ({
+        url: "/",
+      }),
+      providesTags: ["Orders"],
+    }),
+    updateOrderStatus: builder.mutation({
+      query: ({ id, status }) => ({
+        url: `/update-order-status/${id}`,
+        method: "PATCH",
+        body: status,
+      }),
+      invalidatesTags: ["Orders"],
+    }),
+    deleteOrderById: builder.mutation({
+      query: (id) => ({
+        url: `/delete-order/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, id) => [{ type: "Orders", id }],
+    }),
+  }),
+});
+
+export const {
+  useGetOrderByEmailQuery,
+  useGetOrderByIdQuery,
+  useGetAllOrdersQuery,
+  useUpdateOrderStatusMutation,
+  useDeleteOrderByIdMutation,
+} = orderApi;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,6 +4,7 @@ import authReducer from "./features/auth/authSlice";
 import cartReducer from "./features/cart/cartSlice";
 import productsApi from "./features/Products/productsApi";
 import reviewsApi from "./features/Reviews/reviewsApi";
+import { orderApi } from "./features/orders/orderApi";
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
@@ -11,6 +12,7 @@ export const store = configureStore({
     [productsApi.reducerPath]: productsApi.reducer,
     [reviewsApi.reducerPath]: reviewsApi.reducer,
     cart: cartReducer,
+    [orderApi.reducerPath]: orderApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
