cc11ae33ceec2007fe5aac5563518af60c353e27
Md. Hasanujjaman <hasan.hasanujjaman4368@gmail.com>
2025-11-06 12:54:51 +0600

    categoriess

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -4,29 +4,34 @@ import { Link } from "react-router";
 const Categories = () => {
   const categories = [
     {
-      name: "Accessories",
-      path: "accessories",
-      image: "https://i.ibb.co.com/WpPGR1sS/category-1.jpg",
+      name: "Roses",
+      path: "roses",
+      image: "https://i.ibb.co.com/sJ68ckkL/roses.jpg",
     },
     {
-      name: "Dress Collection",
-      path: "dress",
-      image: "https://i.ibb.co.com/7txbcyxp/category-2.jpg",
+      name: "Tulips",
+      path: "tulips",
+      image: "https://i.ibb.co.com/gbGKTVm7/tulips.jpg",
     },
     {
-      name: "Jewellery",
-      path: "jewellery",
-      image: "https://i.ibb.co.com/gFF7y0gL/category-3.jpg",
+      name: "Lilies",
+      path: "lilies",
+      image: "https://i.ibb.co.com/93CjxyC6/lilies.jpg",
     },
     {
-      name: "Cosmetics",
-      path: "cosmetics",
-      image: "https://i.ibb.co.com/nMT6NjPm/category-4.jpg",
+      name: "Orchids",
+      path: "orchids",
+      image: "https://i.ibb.co.com/v4mmXpzm/orchids.jpg",
+    },
+    {
+      name: "Sunflowers",
+      path: "sunflowers",
+      image: "https://i.ibb.co.com/v6DMmv75/sunflowes.jpg",
     },
   ];
 
   return (
-    <div className="max-w-3xl my-20 mx-auto grid grid-cols-2 md:grid-cols-4 gap-5 items-center justify-center">
+    <div className="max-w-4xl my-20 mx-auto grid grid-cols-2 md:grid-cols-5 gap-5 items-center justify-center">
       {categories.map((cat, ind) => (
         <Link key={ind} to={`/category/${cat.path}`}>
           <img
